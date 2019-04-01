d9322a682c901fa3fcd188c48396fc2fda7fb3f9
Chermaraj <mchermaraj@gmail.com>
2019-03-31 19:25:07 -0600

    Committing

---
+++ PredictiveProject/PredictiveAcceptance/patterns/ObserverPattern.py
@@ -0,0 +1,52 @@
+from PredictiveAcceptance.training_models import PredictRegressionModel
+from PredictiveAcceptance.models import StudentProfiles, PredictiveUsers, UniversityAcceptanceRates
+
+
+class studentProfileCore:
+        def __init__(self, user_id):
+                self._observers = []
+                self.user_id = user_id
+ 
+        def attach(self, observer):
+                if observer not in self._observers: 
+                        self._observers.append(observer)
+
+        def detach(self, observer):
+                try:
+                        self._observers.remove(observer)
+                except ValueError:
+                        print("Observer is already not in the list of observers.")
+
+        def notify(self):
+                for observer in self._observers: 
+                        observer.predict(self)
+                 
+        @property
+        def user_id(self):
+                '''The @property decorator makes a property object 'user_id', on which a setter can be defined, as done below.'''
+                return self._user_id
+ 
+        #Setter that sets the temperature of core
+        @user_id.setter
+        def user_id(self, user_id):
+                '''Notify the observers whenever somebody changes the core temperature.
+                   @temp.setter notation signifies the setter method of the temp property which
+                   allows to set the temperature using the assignment operator i.e. objectOfCore.temp = 300
+                   sets the temp attribute to 50'''
+                self._user_id= user_id
+                self.notify()
+                 
+class ProfileMonitoringCore:
+
+  def predict(self, subject):
+   studentId = subject._user_id
+   predictRate = PredictRegressionModel.PredictiveModels()
+   uor_pred,ubc_pred,uoa_pred = predictRate.predictRate(studentId)
+
+   newData = UniversityAcceptanceRates()
+   student_user = PredictiveUsers.objects.get(user_id=studentId)
+   newData.student_user = student_user
+   newData.uor_accep_rate = uor_pred
+   newData.uoa_accep_rate = ubc_pred
+   newData.ubc_accep_rate = uoa_pred
+   newData.save()
--- PredictiveProject/PredictiveAcceptance/views/CreateStudentProfileView.py
+++ PredictiveProject/PredictiveAcceptance/views/CreateStudentProfileView.py
@@ -2,13 +2,18 @@ from django.shortcuts import HttpResponse, render, redirect, get_object_or_404
 from PredictiveAcceptance.forms.CreateStudentProfileForms import CreateStudentProfileForm
 from PredictiveAcceptance.models import StudentProfiles, PredictiveUsers
 from django.contrib import messages
+from PredictiveAcceptance.patterns import ObserverPattern
 
 def StudentProfileCreation(request):  
     username = request.session['username']
     form = CreateStudentProfileForm(request.POST or None)
     userList = PredictiveUsers.objects.filter(username=username).get()
     user_id = PredictiveUsers.objects.values_list('user_id').filter(username=username).get()[0]
-    userObj = get_object_or_404(StudentProfiles ,user=user_id) 
+    if (StudentProfiles.objects.filter(user=user_id).exists()):
+        userObj = get_object_or_404(StudentProfiles ,user=user_id) 
+    else:
+        userObj = PredictiveUsers.objects.filter(username=username).get()
+
     if request.method =='POST':   
   
         if form.is_valid():   
@@ -25,6 +30,15 @@ def StudentProfileCreation(request):
 
             newData.save()
 
+            studentProfile = ObserverPattern.studentProfileCore("Profilecore")
+            profileObserver = ObserverPattern.ProfileMonitoringCore()
+
+            studentProfile.attach(profileObserver)
+            studentProfile.user_id = user_id
+
+
+
+
             system_messages = messages.get_messages(request)
             for message in system_messages:
              pass
@@ -33,9 +47,10 @@ def StudentProfileCreation(request):
             #messages.success(request, 'Your Profile has been created successfully', extra_tags='alert')
             messages.success(request, 'Your Profile has been saved successfully.You can check the acceptance rate', extra_tags='alert')
               
-            return redirect( "/predictAcceptance/", {'user_id':user_id}) 
+            #return redirect( "/predictAcceptance/", {'user_id':user_id}) 
             #return render(request, "PredictiveAcceptance/StudentProfileCreation.html", {'form':form ,'userList':userList})   
-
+            
+            return render(request, "PredictiveAcceptance/StudentProfileCreation.html", {'form':form ,'userList':userList})
 
         else:            
             
